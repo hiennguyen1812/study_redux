adf6d7d9d116cfdca8290b7fb6200376d9ca3ee3
hiennguyen1812 <nguyensihien1812@gmail.com>
2024-04-20 15:24:25 +0700

    20/4

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./container/SideBar/Sidebar";
 import Navbar from "./container/NavBar/Navbar";
 import InforCard from "./InforCard.js/InforCard";
-import HomePage from "./UI/HomePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login";
+import Shop from "./Pages/Shop";
+import ProductList from "./container/Product/ProductList";
+import ShopCategory from "./Pages/ShopCategory";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Login />} />
-        </Routes>
         <Sidebar />
-        <HomePage />
-        <InforCard />
-      </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/about" element={<ShopCategory category="about" />} />
+          <Route path="/service" element={<ShopCategory category="service"  />} />
+          <Route path="/sneaker" element={<ShopCategory category="sneaker"  />} />
+          <Route path="/clothes" element={<ShopCategory category="clothes"  />} />
+          <Route path="/contact" element={<ShopCategory category="contact"  />} />
+          <Route path="/info" element={<InforCard />} />
+          <Route path="/login" element={<Login />} />
+          {/* Add more routes here */}
+        </Routes>
     </div>
   );
 }
